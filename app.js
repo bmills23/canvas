661387e1ae7db5705d6bc36f95d3009f1a4b333a
bmills23 <58231400+bmills23@users.noreply.github.com>
2022-05-23 12:07:06 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas')
+const body = document.querySelector('body')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const c = canvas.getContext('2d')
+
+
+const woah = function () {
+    setInterval(()=> {
+    for (let i = 0; i < 1; i++) {
+    const r = Math.random()*256
+    const g = Math.random()*256
+    const b = Math.random()*256
+    const x = Math.random()*window.innerWidth
+    const y = Math.random()*window.innerHeight
+
+    c.beginPath()
+    c.arc(x, y, 20, 0, 2 * Math.PI)
+    c.strokeStyle = `rgb(${r},${g},${b}`
+    c.lineWidth = 10
+    c.stroke()
+    }
+}, 10)
+    
+}
+
+woah()
